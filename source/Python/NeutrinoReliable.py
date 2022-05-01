4075c2676d4de35426ddce4c2ebf6bb404461e88
etkaar <40885610+etkaar@users.noreply.github.com>
2022-05-01 14:28:11 +0200

    Update license

--- source/Python/NeutrinoReliable.py
+++ source/Python/NeutrinoReliable.py
@@ -2,6 +2,16 @@
 '''
 Copyright (c) 2021–22 etkaar <https://github.com/etkaar/Neutrino>
 
+Restriction (Standard OSPAA 1.0): Only for legal entities with a yearly
+revenue exceeding fifty (50) million US-Dollar (or an equivalent of) the
+license text below is valid with the exception, that in modification of
+the license, for any right granted there (especially the use, modification
+and distribution) the author has the right of fair compensation which must
+be individually agreed. Is the legal entity part of a multinational company,
+the total revenue of all corporations counts. This restriction does not
+apply to non-profit organizations. This text must be included in all
+copies or substantial portions of the source code.
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
