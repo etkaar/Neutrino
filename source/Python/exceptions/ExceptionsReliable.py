3cfd0e85a09bf9b9cc854b9e877c333131b5e4c0
etkaar <40885610+etkaar@users.noreply.github.com>
2022-05-16 23:57:01 +0200

    Create ExceptionsReliable.py

---
+++ source/Python/exceptions/ExceptionsReliable.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+'''
+Copyright (c) 2021–22 etkaar <https://github.com/etkaar/Neutrino>
+
+Restriction (Standard OSPAA 1.0): Only for legal entities with a yearly
+revenue exceeding fifty (50) million US-Dollar (or an equivalent of) the
+license text below is valid with the exception, that in modification of
+the license, for any right granted there (especially the use, modification
+and distribution) the author has the right of fair compensation which must
+be individually agreed. Is the legal entity part of a multinational company,
+the total revenue of all corporations counts. This restriction does not
+apply to non-profit organizations. This text must be included in all
+copies or substantial portions of the source code.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+from typing import Optional
+
+from exceptions.ExceptionsBase import NeutrinoException
+
+class UnrecoverableLoss(NeutrinoException):
+	def __init__(self, message: str, client_id: Optional[int], session_id: int):
+		self.message = message
+		self.client_id = client_id
+		self.session_id = session_id
